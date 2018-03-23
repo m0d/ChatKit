c04746c6f76d0374fe5d3f267ae2636c63d2f5bc
Gregory Pawelczuk <grzegorz.pawelczuk@nftlearning.com>
2018-03-23 13:53:08 +0100

    rx test

--- chatkit/src/main/java/com/stfalcon/chatkit/messages/utils/MessageTextUtils.kt
+++ chatkit/src/main/java/com/stfalcon/chatkit/messages/utils/MessageTextUtils.kt
@@ -6,9 +6,12 @@ import android.text.SpannableString
 import android.text.method.LinkMovementMethod
 import android.text.style.*
 import android.widget.TextView
+import com.github.ajalt.timberkt.w
 import com.stfalcon.chatkit.commons.events.CustomUrlSpan
 import com.stfalcon.chatkit.messages.MarkDown
 import com.stfalcon.chatkit.utils.NonbreakingSpan
+import io.reactivex.Flowable
+import io.reactivex.android.schedulers.AndroidSchedulers
 import java.util.regex.Pattern
 
 /**
@@ -27,14 +30,18 @@ class MessageTextUtils {
 
         private const val QUOTE_INSET = 32
 
-        fun applyTextTransformations(view: TextView, rawText: String, @ColorInt linkColor: Int){
-            //val text  = EmojiTextUtils.transform( rawText )
-            view.text = EmojiTextUtils.transform( rawText ) //MessageTextUtils.transform( text, linkColor )
-            view.movementMethod = LinkMovementMethod.getInstance()
-
+        fun applyTextTransformations(view: TextView, rawText: String, @ColorInt linkColor: Int) {
+            Flowable.fromCallable {
+                val text = EmojiTextUtils.transform(rawText)
+                view.text = MessageTextUtils.transform(text, linkColor)
+                view.movementMethod = LinkMovementMethod.getInstance()
+            }
+            .subscribeOn(io.reactivex.schedulers.Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({},{e -> w{"${e.message}"}})
         }
 
-        private fun transform(text: String, @ColorInt linkColor: Int) : SpannableString{
+        private fun transform(text: String, @ColorInt linkColor: Int): SpannableString {
             return MessageTextUtils.transform(text, getTextPatterns(text), linkColor)
         }
 
@@ -73,8 +80,8 @@ class MessageTextUtils {
                     }
                 }
                 group = group.substring(
-                        if(isQuote) 4 else 1,
-                        group.length - (if(!isQuote) 1 else 0)
+                        if (isQuote) 4 else 1,
+                        group.length - (if (!isQuote) 1 else 0)
                 )
                 if (findNextMarkDown(0, group) != -1) {
                     val reqList = getTextPatterns(group)
@@ -133,7 +140,7 @@ class MessageTextUtils {
                 toReturn = text.substring(1, text.length - 1)
             }
 
-            if(toReturn.startsWith("&gt;")){
+            if (toReturn.startsWith("&gt;")) {
                 toReturn = toReturn.substring("&gt;".length, toReturn.length)
             }
 
@@ -214,7 +221,7 @@ class MessageTextUtils {
             descriptors.forEach { content ->
 
                 if (content.isLink) {
-                    if(content.content.startsWith("https://appear.in")) {
+                    if (content.content.startsWith("https://appear.in")) {
                         spannableString.setSpan(CustomUrlSpan(content.content), content.startIndex, content.endIndex, 0)
                     } else {
                         spannableString.setSpan(URLSpan(content.content), content.startIndex, content.endIndex, 0)
